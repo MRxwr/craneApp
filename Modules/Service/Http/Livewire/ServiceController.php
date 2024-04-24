947935c1337113990c0e392ca4fbd010f774e115
Sonjoy Bhadra <sbhadra0@gmail.com>
2024-04-24 16:17:15 +0530

    Create ServiceController.php

---
+++ Modules/Service/Http/Livewire/ServiceController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Modules\Service\Http\Livewire;
+
+use Illuminate\Contracts\Support\Renderable;
+use Modules\Service\Entities\Service;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class ServiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * @return Renderable
+     */
+    public function index()
+    {
+        //return view('service::index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     * @return Renderable
+     */
+    public function create()
+    {
+        return view('service::liveware.add');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * @param Request $request
+     * @return Renderable
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+       
+        
+        $service = new Service();
+        $service->name = $validatedData['name'];
+        $service->description = $validatedData['description'];
+        if ($request->hasFile('image')) {
+            $imageName = time().'.'.$request->image->extension();  
+            $request->image->move(public_path('services'), $imageName);
+            $service->image = $imageName;
+        }
+        $service->save();
+    
+        // Redirect or return response as needed
+    }
+
+    /**
+     * Show the specified resource.
+     * @param int $id
+     * @return Renderable
+     */
+    public function show($id)
+    {
+        return view('service::show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     * @param int $id
+     * @return Renderable
+     */
+    public function edit($id){
+        $service = Service::findOrFail($id);
+        return view('service::liveware.edit', compact('service'));
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     * @param Request $request
+     * @param int $id
+     * @return Renderable
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @param int $id
+     * @return Renderable
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}

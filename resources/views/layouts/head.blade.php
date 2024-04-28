4655160c6ad9dad7f33c64e87293845b5c251308
Sonjoy Bhadra <sbhadra0@gmail.com>
2024-04-28 14:08:12 +0530

    Update head.blade.php

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -17,8 +17,6 @@
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 
-@if(getLocale()=='ar')
-    <link rel="stylesheet" href="{{ asset('adminlte/dist/css/custom.css') }}">
-@endif
+
 
 @livewireStyles

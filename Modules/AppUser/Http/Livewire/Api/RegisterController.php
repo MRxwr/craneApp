ed0f56dd3edad863c8ae1b318265daa063f9e1fa
Sonjoy Bhadra <sbhadra0@gmail.com>
2024-04-29 18:52:06 +0530

    Update RegisterController.php

--- Modules/AppUser/Http/Livewire/Api/RegisterController.php
+++ Modules/AppUser/Http/Livewire/Api/RegisterController.php
@@ -21,35 +21,41 @@ class RegisterController extends Controller
         $email = $request->input('email');
         $dob = $request->input('dob');
         $password =$request->password;
-        $appuser = AppUser::where('mobile', $mobileNumber)->first();
-        if ($appuser){
-            $appuser->name = $name;
-            $appuser->email = $email;
-            $appuser->mobile = $mobileNumber;
-            $appuser->dob = $dob;
-            $appuser->password = bcrypt($request->password);
-            $appuser->email_verified_at = now();
-            $appuser->save();
-            $data['user']= $appuser->toArray();
-            $data['message']=_lang('Mobile not found');
-            return outputSuccess($data);
+        $isverified = OtpUser::where('mobile', $mobileNumber)->where('verified', 1)->first();
+        if($isverified){
+            $appuser = AppUser::where('mobile', $mobileNumber)->first();
+            if ($appuser){
+                $appuser->name = $name;
+                $appuser->email = $email;
+                $appuser->mobile = $mobileNumber;
+                $appuser->dob = $dob;
+                $appuser->password = bcrypt($request->password);
+                $appuser->email_verified_at = now();
+                $appuser->save();
+                $data['user']= $appuser->toArray();
+                $data['message']=_lang('Mobile not found');
+                return outputSuccess($data);
+            }else{
+                $appuser = new AppUser;
+                $appuser->name = $name;
+                $appuser->email = $email;
+                $appuser->mobile = $mobileNumber;
+                $appuser->dob = $dob;
+                $appuser->password = bcrypt($request->password);
+                $appuser->email_verified_at = now();
+                $appuser->save();
+                $data['user']= $appuser->toArray();
+                $data['message']=_lang('Successfully Regiter'); 
+            }
+            if (auth()->attempt($mobile, $password)) {
+                return outputSuccess($data);
+            } else {
+                $data['message']=_lang('login faild Regiter');
+                return outputError($data);
+            }
         }else{
-            $appuser = new AppUser;
-            $appuser->name = $name;
-            $appuser->email = $email;
-            $appuser->mobile = $mobileNumber;
-            $appuser->dob = $dob;
-            $appuser->password = bcrypt($request->password);
-            $appuser->email_verified_at = now();
-            $appuser->save();
-            $data['user']= $appuser->toArray();
-            $data['message']=_lang('Successfully Regiter'); 
-        }
-        if (auth()->attempt($mobile, $password)) {
-            return outputSuccess($data);
-        } else {
-            $data['message']=_lang('login faild Regiter');
-            return outputError($data);
+            $data['message']=_lang('mobile not  verified');
+            return outputError($data); 
         }
 
     }

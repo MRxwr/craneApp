2175039a7f859936c8651f874ae06e0d520f303b
Sonjoy Bhadra <sbhadra0@gmail.com>
2024-05-10 17:18:53 +0530

    remove module

--- Modules/BookingLogs/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('bookinglogs::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('bookinglogs.name') !!}
-    </p>
-@endsection

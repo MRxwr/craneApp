2a57a7d74e072e457bd78b5c64271ec07b0e54d4
Sonjoy Bhadra <sbhadra0@gmail.com>
2024-05-13 22:05:26 +0530

    Update index.blade.php

--- Modules/BookingRequest/Resources/views/livewire/requests/index.blade.php
+++ Modules/BookingRequest/Resources/views/livewire/requests/index.blade.php
@@ -110,7 +110,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">{{ _lang('Booking Request') }} # {{forms.request_id}}</h5>
+                    <h5 class="modal-title">{{ _lang('Booking Request') }} # {{$forms['request_id']}}</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -124,7 +124,7 @@
                                 {{ $message ?? '' }}
                                 <div class="form-group">
                                     <label for="name">{{ _lang('ID') }}</label>
-                                    {{forms.request_id}}
+                                    {{$forms['request_id']}}
                                 </div>
                                 <div class="form-row">
                                     <div class="form-group col-md-12">

bd3ca4c6fc46057560f1d9f9dec1740298384d8f
Sonjoy Bhadra <sbhadra0@gmail.com>
2024-05-20 17:06:54 +0530

    creatte home api

---
+++ app/Http/Controllers/Api/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Modules\Service\Entities\Service;
+use Modules\Banners\Entities\Banner;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Storage;
+
+class HomeController extends Controller
+{
+    public function getHome()
+    {
+        $services= Service::where('is_active',1)->where('is_deleted',0)
+        ->select('id', 'title', 'description','image') ->get()->toArray();
+        $banners= Banner::where('is_active',1)->where('is_deleted',0)
+        ->select('id', 'title', 'description','image') ->get()->toArray();
+        $data['message']=_lang('Get Home Data');
+        $data['sevices']= $services;
+        $data['banners']= $banners;
+        return outputSuccess($data);
+        
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\HomeController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// API Version 1 routes
+Route::prefix('v1')->group(function () {
+    Route::get('/home', [HomeController::class, 'getHome']);
 });

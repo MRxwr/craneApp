fb09502eabb391b41aa6deb1ed358f1e61888305
Sonjoy Bhadra <sbhadra0@gmail.com>
2024-05-21 22:18:32 +0530

    test

--- Modules/Coupons/Resources/views/livewire/coupon/edit.blade.php
+++ Modules/Coupons/Resources/views/livewire/coupon/edit.blade.php
@@ -7,7 +7,7 @@
         <div class="card-body row">
 
             <div class=" col-md-6 modal-content">
-            <form action="{{ route('banners.update', $service->id) }}" method="POST" enctype="multipart/form-data">
+            <form action="{{ route('coupons.update', $service->id) }}" method="POST" enctype="multipart/form-data">
                     @csrf
                     @method('PUT') <!-- Use PUT method for updating -->
                             <div class="card-body">

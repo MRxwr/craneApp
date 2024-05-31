e4d9bd5556586927db9f12152b2a630338eba958
Sonjoy Bhadra <sbhadra0@gmail.com>
2024-06-01 00:31:08 +0530

    Update general.blade.php

--- Modules/Notifications/Resources/views/livewire/notification/general.blade.php
+++ Modules/Notifications/Resources/views/livewire/notification/general.blade.php
@@ -108,7 +108,7 @@
     </div>
 
     <div class="modal fade" tabindex="-1" role="dialog" id="modalAdd" wire:ignore.self>
-        <div class="modal-dialog lg-modal" role="document">
+        <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">{{_lang('Add Notice')}}</h5>

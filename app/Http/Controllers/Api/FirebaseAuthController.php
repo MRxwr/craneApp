492075003bcb3d3b0681e6dec9a099d8487967cb
Sonjoy Bhadra <sbhadra0@gmail.com>
2024-09-18 00:10:38 +0530

    test

--- app/Http/Controllers/Api/FirebaseAuthController.php
+++ app/Http/Controllers/Api/FirebaseAuthController.php
@@ -1,38 +1,32 @@
 <?php
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller; // Import the correct Controller class
-use Illuminate\Http\Request;
-use Kreait\Firebase\Factory;
-use Kreait\Firebase\Auth;
+use App\Http\Controllers\Controller;
+use App\Services\FirebaseService;
+use Illuminate\Http\JsonResponse;
 
 class FirebaseAuthController extends Controller
 {
-    protected $firebaseAuth;
-    public function __construct()
-    {
-       // Initialize Firebase with the service account
-       $firebase = (new Factory)
-            ->withServiceAccount("/home/u341645071/domains/trycraneapp.com/public_html/admincp/storage/app/fcm/crane-dbb8b-firebase-adminsdk-uezkp-e824b13302.json");
+    protected $firebaseService;
 
-       $this->firebaseAuth = $firebase->createAuth();
+    public function __construct(FirebaseService $firebaseService)
+    {
+        $this->firebaseService = $firebaseService;
     }
 
-    // Function to verify Firebase ID Token
+    /**
+     * Get the Bearer Token and return it as JSON response.
+     *
+     * @return JsonResponse
+     */
     public function getBearerToken()
     {
-        dd(config('firebase.credentials.file'));
-         // Example: Authenticate a user and get an ID token (Bearer token)
-         $email = 'Createkwco@gmail.com'; // Replace with your Firebase user's email
-         $password = 'N@b$90949089'; // Replace with the user's password
-         
-         // Sign in the user with email and password
-         $signInResult = $this->firebaseAuth->signInWithEmailAndPassword($email, $password);
-         dd($signInResult);
-         // Get the Bearer Token (Firebase ID Token)
-         $idToken = $signInResult->idToken();  // This is the Bearer Token
-        
-         return response()->json(['token' => $idToken]);
+        $token = $this->firebaseService->getBearerToken();
+
+        if ($token) {
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['error' => 'Unable to get token'], 500);
+        }
     }
-    
 }

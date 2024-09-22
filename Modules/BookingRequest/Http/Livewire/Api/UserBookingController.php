b6e05af9dc9d2668e9943c90a66b4a2f349f8021
Sonjoy Bhadra <sbhadra0@gmail.com>
2024-09-22 23:40:32 +0530

    Update UserBookingController.php

--- Modules/BookingRequest/Http/Livewire/Api/UserBookingController.php
+++ Modules/BookingRequest/Http/Livewire/Api/UserBookingController.php
@@ -52,7 +52,7 @@ class UserBookingController extends Controller
                $prices=[];
                 foreach ($dt as $key=>$bookingRequest){
                     $createdAt = $bookingRequest->created_at;
-                    $hour = $createdAt->format('g'); // Get the hour in 12-hour format without leading zeros
+                    $hour = @$createdAt->format('g'); // Get the hour in 12-hour format without leading zeros
                     $hour = str_pad($hour, 2, '0', STR_PAD_LEFT);
                         // Format the rest
                         $formattedDate = $createdAt->format('dM ') . $hour . $createdAt->format(':iA');

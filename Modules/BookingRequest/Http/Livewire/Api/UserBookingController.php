5f82f66737342b2107b2e5f6110c9e7ae3c45a9c
Sonjoy Bhadra <sbhadra0@gmail.com>
2024-11-12 17:22:17 +0530

    Update UserBookingController.php

--- Modules/BookingRequest/Http/Livewire/Api/UserBookingController.php
+++ Modules/BookingRequest/Http/Livewire/Api/UserBookingController.php
@@ -219,7 +219,7 @@ class UserBookingController extends Controller
             // $totalDistance = $todayRequests->sum('distances'); // Assuming 'distance' is a field in BookingRequest
             // $totalRequests = $todayRequests->count();
             // Calculate the total distance for successful payments
-            $totalDistance = $successfulRequests->sum('distance');
+            //$totalDistance = $successfulRequests->sum('distance');
             // Count the total number of requests with successful payments
             $totalRequests = $successfulRequests->count();
                $data['todayEarnings']= [

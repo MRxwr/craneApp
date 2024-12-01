46495b0ceaee9b7d07ba86720e5990f4d434b361
Sonjoy Bhadra <sbhadra0@gmail.com>
2024-12-01 22:52:27 +0530

    Update BookingController.php

--- Modules/BookingRequest/Http/Livewire/Api/BookingController.php
+++ Modules/BookingRequest/Http/Livewire/Api/BookingController.php
@@ -840,7 +840,7 @@ class BookingController extends Controller
                 'extraMerchantData[1][ccChargeType]' => 'fixed',
                 'extraMerchantData[1][ibanNumber]' => "{$driver_iban}",
                 );
-        var_dump($postBody);exit;
+        
             $curl = curl_init();
             curl_setopt_array($curl, array(
                 CURLOPT_URL => 'https://uapi.upayments.com/api/v1/charge',
